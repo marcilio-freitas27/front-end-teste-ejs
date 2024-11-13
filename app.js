ffd61915b46ce88fb0ef4884e70497b5027505ca
Marcílio Freitas <marcilio@beltsistemas.com.br>
2024-11-13 14:10:10 -0300

    Feature | Remove vercel, env and routes configurations.

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-// require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = 3000;
-const indexRoute = require('./routes/index');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
